280fa5b7f4c94db13e101e53b02515e50f075c3b
Baman Irani <amany29074@gmail.com>
2024-06-03 22:58:22 +0530

    complete

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+// tailwind.config.js
+
+module.exports = {
+  content: ["./dist/*.{html,js}"],
+  theme: {
+   extend: {
+      fontFamily: {
+        'clash-display': ['Clash Display', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [],
+};
